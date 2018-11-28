d51ce231dd77532f40788ac57526a91fe5f84165
RFyutian <458671456@qq.com>
2018-11-28 20:55:57 +0800

    Update lcd_driver.v

--- lcd_top/lcd_driver.v
+++ lcd_top/lcd_driver.v
@@ -14,25 +14,26 @@ Data			By			Version			Change Description
 --------------------------------------------------------------------------*/
 module lcd_driver
 (  	
-  //global clock
-  input			clk,			//system clock
-  input			rst_n,     		//sync reset
+	//global clock
+	input			clk,			//system clock
+	input			rst_n,     		//sync reset
 
-  //lcd interface
-  output			lcd_dclk,   	//lcd pixel clock
-  output			lcd_blank,		//lcd blank
-  output			lcd_sync,		//lcd sync
-  output			lcd_hs,	    	//lcd horizontal sync
-  output			lcd_vs,	    	//lcd vertical sync
-  output			lcd_en,			//lcd display enable
-  output	[15:0]	lcd_rgb,		//lcd display data
+	//lcd interface
+	output			lcd_dclk,   	//lcd pixel clock
+//	output			lcd_blank,		//lcd blank
+//	output			lcd_sync,		//lcd sync
+	output			lcd_hs,	    	//lcd horizontal sync
+	output			lcd_vs,	    	//lcd vertical sync
+	output			lcd_en,			//lcd display enable
+	output	[23:0]	lcd_rgb,		//lcd display data
 
 	//user interface
+	input			lcd_rd_en,
 	output			lcd_request,	//lcd data request
 	output			lcd_framesync,	//lcd frame sync
 	output	[10:0]	lcd_xpos,		//lcd horizontal coordinate
 	output	[10:0]	lcd_ypos,		//lcd vertical coordinate
-	input	[15:0]	lcd_data		//lcd data
+	input	[23:0]	lcd_data		//lcd data
 );	 
 `include "lcd_para.v"  
 
@@ -76,12 +77,12 @@ assign	lcd_vs = (vcnt <= `V_SYNC - 1'b1) ? 1'b0 : 1'b1;
 //------------------------------------------
 //LCELL	LCELL(.in(clk),.out(lcd_dclk));
 assign	lcd_dclk = ~clk;
-assign	lcd_blank = lcd_en;//lcd_hs & lcd_vs;		
-assign	lcd_sync = 1'b0;
+//assign	lcd_blank = lcd_en;//lcd_hs & lcd_vs;		
+//assign	lcd_sync = 1'b0;
 assign	lcd_en		=	(hcnt >= `H_SYNC + `H_BACK  && hcnt < `H_SYNC + `H_BACK + `H_DISP) &&
 						(vcnt >= `V_SYNC + `V_BACK  && vcnt < `V_SYNC + `V_BACK + `V_DISP) 
 						? 1'b1 : 1'b0;
-assign	lcd_rgb 	=  (lcd_en && lcd_request) ? lcd_data : 16'd0;
+assign	lcd_rgb 	=  (lcd_en && lcd_request) ? lcd_data : 24'd0;
 assign	lcd_framesync = lcd_vs;
 
 
