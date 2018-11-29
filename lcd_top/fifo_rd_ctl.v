563447728f77e28fac53561d5ec8119f7f81a269
RFyutian <458671456@qq.com>
2018-11-29 19:45:46 +0800

    Update fifo_rd_ctl.v

--- lcd_top/fifo_rd_ctl.v
+++ lcd_top/fifo_rd_ctl.v
@@ -9,7 +9,7 @@ module fifo_rd_ctl#(
 	fifo_empty,
 	fifo_rd_cnt,
 	//lcd interface
-	lcd_data_requst
+	rd_data_requst
 );
 //--------------------------------------------------------------------------//
 	//interface 
@@ -21,14 +21,36 @@ module fifo_rd_ctl#(
 	input					fifo_empty;	
 	input 		[9 : 0]		fifo_rd_cnt;
 	//lcd interface
-	input 					lcd_data_requst;
+	input 					rd_data_requst;
 //--------------------------------------------------------------------------//
 
 	//wire rd_ready;
 	reg rd_ready;
+	reg data_requst_dly1;
+	reg data_requst_dly2;
+	
+	wire data_requst;
 
 //assign rd_ready = ((fifo_rd_cnt>FIFO_ALMOSTEMPTY_DEPTH)&&(!fifo_empty))?1:0;	//若fifo非空，且数据量大于最小数据深度时，可读，高有效
+//打一拍在读，否则会出现亚稳态
+always@(posedge fifo_rd_clk)begin
+	if(!rst_n)begin
+		data_requst_dly1 <= 0;
+		data_requst_dly2 <= 0;
+	end
+	else 
+	begin 
+//		if(rd_data_requst)
+		begin
+			data_requst_dly1 <= rd_data_requst;
+			data_requst_dly2 <= data_requst_dly1; 
+		end
+	end
+end
 
+//assign data_requst = data_requst_dly1&&rd_data_requst;
+assign data_requst = data_requst_dly1;
+//assign data_requst = data_requst_dly1&&data_requst_dly2;
 
 always@(posedge fifo_rd_clk)begin
 	if(!rst_n)begin
@@ -46,38 +68,7 @@ always@(posedge fifo_rd_clk)begin
 	end
 end
 	
-assign fifo_rd_en = (rd_ready&&lcd_data_requst&&rst_n)?1:0;			//若读就绪且有数据请求且未复位时，读使能输出为1
-	
-//状态机方式实现：
-/*
-parameter IDLE = 0;
-parameter BUSY = 1;
-
-reg [1:0] 			status;
-
-always@(posedge fifo_rd_clk)begin
-	if(!rst_n)begin
-		status = IDLE;
-	end
-	else 
-	begin
-		case (status)
-			IDLE:
-			begin
-				
-			end
-			BUSY:
-			begin
-			
-			end
-			default:
-			begin
-			
-			end
-		endcase
-	end
-end
-*/
+assign fifo_rd_en = (rd_ready&&data_requst&&rst_n)?1:0;			//若读就绪且有数据请求且未复位时，读使能输出为1
 
 endmodule
 
