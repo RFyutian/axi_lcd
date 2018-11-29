83fb1721f32eeee3c8b94f60f6c010ed2847d134
RFyutian <458671456@qq.com>
2018-11-29 19:45:30 +0800

    Update fifo_ctl_top.v

--- lcd_top/fifo_ctl_top.v
+++ lcd_top/fifo_ctl_top.v
@@ -7,7 +7,6 @@ module fifo_ctl_top #(
 	rst_n,							//input		系统复位
 	//axi stream interface 
 	axis_data_en,					//input axi stream 数据输出使能
-	axis_data_sync,  				//input axi stream 数据同步
 	axis_data_requst,				//output axi stream  数据请求
 	//fifo port
 		//read
@@ -21,8 +20,7 @@ module fifo_ctl_top #(
 	fifo_full,						//input		fifo满指示
 	fifo_wr_cnt,					//input		fifo写计数器
 	//lcd driver port
-	lcd_framesync,					//output 	帧同步输出
-	lcd_data_requst					//input	数据请求输入
+	rd_data_requst					//input	数据请求输入
 );
 
 //--------------------------------------------------------------------------//
@@ -31,7 +29,6 @@ module fifo_ctl_top #(
 	input 					rst_n;
 	//axi stream
 	input 					axis_data_en;
-	input 					axis_data_sync;
 	output  				axis_data_requst;
 	//fifo
 		//read
@@ -45,8 +42,7 @@ module fifo_ctl_top #(
 	input 					fifo_full;
 	input		[9 : 0]		fifo_wr_cnt;
 	//lcd
-	output 					lcd_framesync;
-	input					lcd_data_requst;
+	input					rd_data_requst;
 //--------------------------------------------------------------------------//
 
 fifo_wr_ctl #(
@@ -57,19 +53,16 @@ fifo_wr_ctl #(
 	.rst_n				(rst_n),
 	//axi stream
 	.axis_data_en		(axis_data_en),
-	.axis_data_sync		(axis_data_sync),
 	.axis_data_requst	(axis_data_requst),
 	//fifo write
 	.fifo_wr_clk		(fifo_wr_clk),
 	.fifo_wr_en			(fifo_wr_en),
 	.fifo_full			(fifo_full),
-	.fifo_wr_cnt		(fifo_wr_cnt),
-	//lcd
-	.lcd_framesync		(lcd_framesync)
+	.fifo_wr_cnt		(fifo_wr_cnt)
 );	
 
 fifo_rd_ctl #(
-	.FIFO_ALMOSTEMPTY_DEPTH(FIFO_ALMOSTEMPTY_DEPTH)
+	.FIFO_ALMOSTEMPTY_DEPTH(32)				//11-29修改
 )u_fifo_rd_ctl(
 	//system
 	.rst_n				(rst_n),
@@ -79,7 +72,7 @@ fifo_rd_ctl #(
 	.fifo_empty			(fifo_empty),
 	.fifo_rd_cnt		(fifo_rd_cnt),
 	//lcd interface
-	.lcd_data_requst	(lcd_data_requst)
+	.rd_data_requst		(rd_data_requst)
 );
 
 endmodule
